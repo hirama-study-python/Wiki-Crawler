18ccf521732a46ce1c32aeae05555e31e19aa27c
平間和樹 <kh022427@gmail.com>
2019-03-05 22:35:42 +0900

    elasticsearchをAWSにのっける

--- WikiCrawler/pipelines.py
+++ WikiCrawler/pipelines.py
@@ -6,13 +6,36 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, RequestsHttpConnection
 from elasticsearch.exceptions import NotFoundError
+from requests_aws4auth import AWS4Auth
+import boto3
 
 class ElasticsearchPipeline(object):
 
     def open_spider(self, spider):
-        self.es = Elasticsearch()
+
+        # 構築したElasticseachのドメイン情報
+        host = 'search-kazuki-hirama-7lw5kfmmp5mbk2tniaddbaftpa.us-east-1.es.amazonaws.com'
+        region = 'us-east-1'
+
+        service = 'es'
+
+        # 環境変数からAWSへの認証情報を作成する
+        credentials = boto3.Session().get_credentials()
+        awsauth = AWS4Auth( credentials.access_key , credentials.secret_key , region , service )
+
+        # クライアントをAWS Elasticsearchに接続　
+
+        self.es = Elasticsearch(
+            hosts=[{'host': host, 'port': 443}],
+            http_auth=awsauth,
+            use_ssl=True,
+            verify_certs=True,
+            connection_class=RequestsHttpConnection
+        )
+
+
 
     def process_item(self, item, spider):
         # 更新分のニュースを変数に代入
